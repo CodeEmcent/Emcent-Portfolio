ab828a2cb10c8b407d095d771fa7db9a46df5ccf
Emekwue Innocent <mcinnobezzy@gmail.com>
2023-08-18 22:53:07 +0100

    Sidebar and Home Components Added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React from 'react'
 import './home.css'
+import Me from '../../assets/avatar-1.svg'
+import HeaderSocial from './HeaderSocial'
 
-const Home =()=>{
-    return(
-        <div>Home</div>
-    )
+const Home = () => {
+  return (
+    <section className='home container' id='home'>
+      <div className='intro'>
+        <img src={Me} alt='' className='home__img' />
+        <h1 className='home__name'>Emekwue Innocent</h1>
+        <span className='home__education'>I'm a Front-End Developer</span>
+
+        <HeaderSocial />
+      </div>
+    </section>
+  )
 }
-
 
 export default Home
