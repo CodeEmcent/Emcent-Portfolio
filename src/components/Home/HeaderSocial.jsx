15d2fd3c71d8f4a3ddb6fdec1450711c77cb1571
Emekwue Innocent <mcinnobezzy@gmail.com>
2023-08-20 00:01:30 +0100

    Home Component Added

--- src/components/Home/HeaderSocial.jsx
+++ src/components/Home/HeaderSocial.jsx
@@ -3,27 +3,43 @@ import React from 'react'
 const HeaderSocial = () => {
   return (
     <div className='home__socials'>
-      <a href='' className='home__social-link' target='_blank'>
+      <a
+        href='https://www.instagram.com/'
+        className='home__social-link'
+        target='_blank'
+      >
         <i className='fa-brands fa-instagram'></i>
       </a>
 
-      <a href='' className='home__social-link' target='_blank'>
+      <a
+        href='https://www.twitter.com/'
+        className='home__social-link'
+        target='_blank'
+      >
         <i className='fa-brands fa-twitter'></i>
       </a>
 
-      <a href='' className='home__social-link' target='_blank'>
-        <i className='fa-brands fa-behance'></i>
-      </a>
-
-      <a href='' className='home__social-link' target='_blank'>
+      <a
+        href='https://www.dribble.com/'
+        className='home__social-link'
+        target='_blank'
+      >
         <i className='fa-brands fa-dribbble'></i>
       </a>
 
-      <a href='' className='home__social-link' target='_blank'>
+      <a
+        href='https://www.pinterest.com/'
+        className='home__social-link'
+        target='_blank'
+      >
         <i className='fa-brands fa-pinterest'></i>
       </a>
 
-      <a href='' className='home__social-link' target='_blank'>
+      <a
+        href='https://www.facebook.com/'
+        className='home__social-link'
+        target='_blank'
+      >
         <i className='fa-brands fa-facebook'></i>
       </a>
     </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,16 +2,23 @@ import React from 'react'
 import './home.css'
 import Me from '../../assets/avatar-1.svg'
 import HeaderSocial from './HeaderSocial'
+import ScrollDown from './ScrollDown'
 
 const Home = () => {
   return (
     <section className='home container' id='home'>
       <div className='intro'>
-        <img src={Me} alt='' className='home__img' />
+        <img src={Me} alt='MyPicture' className='home__img' />
         <h1 className='home__name'>Emekwue Innocent</h1>
         <span className='home__education'>I'm a Front-End Developer</span>
 
         <HeaderSocial />
+
+        <a href='#contact' className='btn'>
+          Hire Me
+        </a>
+
+        <ScrollDown />
       </div>
     </section>
   )
