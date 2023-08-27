3ba6a55fe53e33d6b7e7ea1c24862bba1e31af96
Emekwue Innocent <mcinnobezzy@gmail.com>
2023-08-27 22:00:30 +0100

    Portfolio Completed

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
   const [toggle, showMenu] = useState(false)
   return (
     <>
-      <aside className='aside'>
+      <aside className={toggle ? 'aside show-menu' : 'aside'}>
         <a href='#home' className='nav__logo'>
           <img src={Logo} alt='emcent_logo' />
         </a>
@@ -63,7 +63,10 @@ const Sidebar = () => {
         </div>
       </aside>
 
-      <div className={toggle ? 'nav__toggle nav__toggle-open' : 'nav__toggle'}>
+      <div
+        className={toggle ? 'nav__toggle nav__toggle-open' : 'nav__toggle'}
+        onClick={() => showMenu(!toggle)}
+      >
         <i className='icon-menu'></i>
       </div>
     </>
